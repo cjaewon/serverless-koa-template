d4e1716ec111365b124620fba16cf1e962e713f3
cjaewon <jwn4492@outlook.com>
2020-08-02 14:16:33 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import Koa from 'koa';
+import Router from '@koa/router';
 
 const app = new Koa();
+const router = new Router();
+
+router.get('/hello-world', ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
 
 export default app;
 
